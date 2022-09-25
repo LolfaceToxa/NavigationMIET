a747a15d70d2799a7c072e9dca046287713ebaa9
4z0t <gastec139@gmail.com>
2022-09-25 19:51:08 +0300

    init

---
+++ Assets/Pathing/PathNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Pathing
+{
+    class PathNode : Node
+    {
+        Dictionary<double, Node> Connections { get; set; }
+
+        public PathNode()
+        {
+            this.Connections = new Dictionary<double, Node>();
+        }
+        
+        
+    }
+}
