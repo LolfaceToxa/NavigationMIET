a3afd3bc7fd2eb46328f6f72f1f30a1ea8fa0ca7
4z0t <gastec139@gmail.com>
2022-11-07 17:52:44 +0300

    WIP

--- Assets/Pathing/PathGraph.cs
+++ Assets/Pathing/PathGraph.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,13 +23,79 @@ namespace Assets.Pathing
                 Length = Vector3.Distance(b.Position, a.Position);
             }
 
+            public 
+
         }
 
-        List<PathNode> _nodes;
+        public class LinkEnum : IEnumerator
+        {
+
+            List<Link> _links;
+            PathNode _node;
+
+            int position = -1;
+
+            public LinkEnum(List<Link> links, PathNode node)
+            {
+                _links = links;
+                _node = node;
+            }
+
+            public Link Current
+            {
+                get
+                {
+                    try
+                    {
+                        return _links[position];
+                    }
+                    catch(IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+
+
+            public bool MoveNext()
+            {
+                while()
+                position++;
+
+                return (position < _links.Count);
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+
+        HashSet<PathNode> _nodes;
         List<Link> _links;
 
         public PathGraph()
         {
+
+        }
+
+
+        public void Add(PathNode node)
+        {
+
+        }
+
+        public void Add(PathNode a, PathNode b)
+        {
+            if (!_nodes.Contains(a))
+            {
+                Add(a);
+            }
+            if (!_nodes.Contains(b))
+            {
+                Add(b);
+            }
+
 
         }
 
